42524d85b839d2ba897d8a6024248d29fd37011e
Belova <sonia333@yandex.ru>
2022-11-05 15:19:45 +0700

    exs3,4 done

--- ex4.java
+++ ex4.java
@@ -1,14 +1,17 @@
 public class ex4 {
-    class Solution {
-        public int removeElement(int[] nums, int val) {
-           int k = 0;
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] != val) {
-                nums[k++] = nums[i];
-                }
+    public static void main(String[] args) {
+        int[] nums = { 0,1,2,2,3,0,4,2 };
+        int result = removeElement(nums, 2);
+        System.out.println(result);
+    }
+    public static int removeElement(int [] nums, int val) {
+        int k = 0;
+           for (int i = 0; i < nums.length; i++) {     
+           if (nums[i] != val) {
+           nums[k++] = nums[i];
             }
-             return k;
-        }
-       
+        } 
+        return k;
     }
 }
+        
